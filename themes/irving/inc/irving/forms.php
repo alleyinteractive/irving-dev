9755d0a192044d134b4d7b2859a228769c4cd4a0
Owen Stowe <ostowe@gmail.com>
2018-10-30 21:22:07 -0400

    Add demo contact form

---
+++ themes/irving/inc/irving/forms.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Irving implementation.
+ *
+ * @package Irving
+ */
+
+namespace Irving;
+
+
+/**
+ * Setup form endpoints and callbacks
+ *
+ * @param  array $form_endpoints Form endpoint slugs and callback functions.
+ * @return array Form endpoints with Conensys forms merged in.
+ */
+function form_endpoints( $form_endpoints ) {
+	$form_endpoints[] = [
+		'slug' => 'contact',
+		'callback' => [ __NAMESPACE__ . '\Contact_Form', 'get_route_response' ],
+	];
+
+	return $form_endpoints;
+}
+add_filter( 'wp_irving_form_endpoints',  __NAMESPACE__ . '\form_endpoints', 10, 1 );
