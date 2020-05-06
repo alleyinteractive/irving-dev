45ca48e7a5939ce0cf70a518390398c2c42acd99
Amy Evans <amy@alley.co>
2020-05-05 20:49:44 -0500

    Remove the example body component from the component map

--- private/irving/componentMap.js
+++ private/irving/componentMap.js
@@ -1,9 +1,7 @@
-import Body from 'example-components/layouts/body';
 import Footer from 'example-components/layouts/footer';
 import Header from 'example-components/layouts/header';
 
 export default {
-  body: Body,
   footer: Footer,
   header: Header,
 };
