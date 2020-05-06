6269c5c9092a32cfe05bbf26f46551874c77d27e
Amy Evans <amy@alley.co>
2020-05-05 19:12:55 -0500

    Scaffold Irving app

---
+++ private/irving/componentMap.js
@@ -0,0 +1,9 @@
+import Body from 'example-components/layouts/body';
+import Footer from 'example-components/layouts/footer';
+import Header from 'example-components/layouts/header';
+
+export default {
+  body: Body,
+  footer: Footer,
+  header: Header,
+};
---
+++ private/irving/readme.md
@@ -0,0 +1,2 @@
+### Irving Dev App
+
