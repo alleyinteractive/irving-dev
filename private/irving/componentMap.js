8733d56dbc9dbd22d84255768dd4e90261d23d5d
James Burke <james.walter.burke@gmail.com>
2020-05-19 12:44:14 -0500

    Update component map

--- private/irving/componentMap.js
+++ private/irving/componentMap.js
@@ -2,29 +2,27 @@ import * as materialComponents from '@material-ui/core';
 import * as materialLabComponents from '@material-ui/lab';
 import Fragment from 'component-candidates/fragment';
 
-const mapping = {
+const transformName = (original) => original
+  .replace(/(^[A-Z])/, ([first]) => first.toLowerCase())
+  .replace(/([A-Z])/g, ([letter]) => `-${letter.toLowerCase()}`);
+
+export default {
   '': Fragment,
   'irving/fragment': Fragment,
   'irving/passthrough': Fragment,
   'irving/body-wrapper': Fragment,
   'irving/footer-wrapper': Fragment,
   'irving/header-wrapper': Fragment,
+  ...Object.keys(materialComponents)
+    .reduce((acc, key) => {
+      acc[`material/${transformName(key)}`] = materialComponents[key];
+      return { ...acc };
+    },
+    {}),
+  ...Object.keys(materialLabComponents)
+    .reduce((acc, key) => {
+      acc[`material-lab/${transformName(key)}`] = materialComponents[key];
+      return { ...acc };
+    },
+    {}),
 };
-
-Object.keys(materialComponents).forEach((index) => {
-  const name = index
-    .replace(/(^[A-Z])/, ([first]) => first.toLowerCase())
-    .replace(/([A-Z])/g, ([letter]) => `-${letter.toLowerCase()}`);
-
-  mapping[`material/${name}`] = materialComponents[index];
-});
-
-Object.keys(materialLabComponents).forEach((index) => {
-  const name = index
-    .replace(/(^[A-Z])/, ([first]) => first.toLowerCase())
-    .replace(/([A-Z])/g, ([letter]) => `-${letter.toLowerCase()}`);
-
-  mapping[`material-lab/${name}`] = materialLabComponents[index];
-});
-
-export default mapping;
--- themes/irving-example/index.php
+++ themes/irving-example/index.php
@@ -5,22 +5,11 @@
  * @package Irving_Example
  */
 
-?>
-
-<?php
-if ( is_home() && ! is_front_page() ) :
-	?>
-	<header>
-		<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-	</header>
-	<?php
-endif;
-
-while ( have_posts() ) :
-	the_post();
-
-	get_template_part( 'template-parts/content', get_post_type() );
-
-endwhile;
-
-the_posts_navigation();
+// phpcs:ignore Squiz.Commenting.FileComment.SpacingAfterComment
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<?php wp_head(); ?>
+<body <?php body_class(); ?>>
+<?php wp_footer(); ?>
+</body>
+</html>
