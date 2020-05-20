0d364dba7f943684157d04c10496160ab9c765a7
James Burke <james.walter.burke@gmail.com>
2020-05-20 16:55:34 -0500

    Cleanup for code review

--- private/irving/componentMap.js
+++ private/irving/componentMap.js
@@ -2,31 +2,12 @@ import { Helmet } from 'react-helmet';
 import * as materialComponents from '@material-ui/core';
 import * as materialLabComponents from '@material-ui/lab';
 import * as materialIconsComponents from '@material-ui/icons';
-import App from 'component-candidates/layouts/app';
-import Fragment from 'component-candidates/common/fragment';
-import Logo from 'component-candidates/modules/logo';
-import Script from 'component-candidates/common/script';
-import Menu from 'component-candidates/wordpress/site-menu';
-import MenuItem from 'component-candidates/wordpress/site-menu-item';
-// import Text from 'component-candidates/common/text';
 
 const transformName = (original) => original
   .replace(/(^[A-Z])/, ([first]) => first.toLowerCase())
   .replace(/([A-Z])/g, ([letter]) => `-${letter.toLowerCase()}`);
 
 export default {
-  '': Fragment,
-  'irving/body-wrapper': Fragment,
-  'irving/footer-wrapper': Fragment,
-  'irving/fragment': Fragment,
-  'irving/header-wrapper': Fragment,
-  'irving/helmet': Helmet,
-  'irving/logo': Logo,
-  'site/menu': Menu,
-  'site/menu-item': MenuItem,
-  'irving/passthrough': Fragment,
-  'irving/script': Script,
-  'irving/text': Fragment,
   ...Object.keys(materialComponents)
     .reduce((acc, key) => {
       acc[`material/${transformName(key)}`] = materialComponents[key];
@@ -45,5 +26,4 @@ export default {
       return { ...acc };
     },
     {}),
-  app: App,
 };
