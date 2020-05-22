628263cfe3cb59f310b4d90b21f4e9e020380f87
James Burke <james.walter.burke@gmail.com>
2020-05-22 17:59:18 -0500

    Adjust css

--- private/irving/component-candidates/modules/menu/themes/defaultVertical.js
+++ private/irving/component-candidates/modules/menu/themes/defaultVertical.js
@@ -10,6 +10,7 @@ export const Wrapper = styled.div`
 // Wrapper around the menu name.
 export const NameWrapper = styled.h3`
   font-weight: 600;
+  font-size: 1.25rem;
   padding-bottom: 1rem;
 `;
 
@@ -23,14 +24,15 @@ export const Inner = styled.ul`
 
 export const ItemWrapper = styled.span`
   flex: 1 0 auto;
-  margin-bottom: .5rem;
+  margin-bottom: .2rem;
   padding: 0;
 
   li {
     a {
       color: #12121c;
+      display: block;
       font-size: 1rem;
-      padding: 0 0 1rem 0;
+      padding: 0 0 .5rem 0;
       text-decoration: none;
     }
 
