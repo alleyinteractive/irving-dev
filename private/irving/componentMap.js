07706c8e7292993d354df0ff46c34a2bd4aa8880
James Burke <james.walter.burke@gmail.com>
2020-05-27 22:35:54 -0500

    Update component map further

--- private/irving/componentMap.js
+++ private/irving/componentMap.js
@@ -2,22 +2,17 @@ import { Helmet } from 'react-helmet';
 import * as materialComponents from '@material-ui/core';
 import App from 'component-candidates/layouts/app';
 import Fragment from 'component-candidates/layouts/fragment';
-import HTML from 'component-candidates/common/html';
 import Logo from 'component-candidates/modules/logo';
 import Menu from 'component-candidates/modules/menu';
 
-// Icons.
+// Icons
 import { Search as SearchIcon } from '@material-ui/icons';
-
-// Beta components.
-const betaComponentMapping = {};
 
 const transformName = (original) => original
   .replace(/(^[A-Z])/, ([first]) => first.toLowerCase())
   .replace(/([A-Z])/g, ([letter]) => `-${letter.toLowerCase()}`);
 
 export default {
-  ...betaComponentMapping,
   '': Fragment,
   'irving/body-wrapper': Fragment,
   'irving/footer-wrapper': Fragment,
@@ -26,8 +21,6 @@ export default {
   'irving/helmet': Helmet,
   'irving/logo': Logo,
   'irving/menu': Menu,
-  'irving/post': Fragment,
-  'irving/post-featured-media': Fragment,
   'irving/post-list': Fragment,
   'material-icon/search': SearchIcon,
   app: App,
