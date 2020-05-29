d43bc11be88577d645a8e8a0c44bb75f0665323d
James Burke <james.walter.burke@gmail.com>
2020-05-28 19:38:12 -0500

    Code review suggestions

--- private/irving/component-candidates/wordpress/post/excerpt/index.php
+++ private/irving/component-candidates/wordpress/post/excerpt/index.php
@@ -35,7 +35,7 @@ get_registry()->register_component_from_config(
 				->set_config( 'content', html_entity_decode( get_the_excerpt( $post_id ) ) )
 				// Temporarily map this to irving/text so it gets converted to
 				// a text dom node upon render.
-				->set_name( 'irving/text' );
+				->set_name( 'irving/html' );
 		},
 	]
 );
