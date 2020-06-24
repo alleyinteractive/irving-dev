8d8c923311c4fed8c06af42badc857dd60c9744b
James Burke <james.walter.burke@gmail.com>
2020-06-23 23:23:22 -0400

    More progress

--- private/irving/componentMap.js
+++ private/irving/componentMap.js
@@ -1,24 +1,13 @@
 import { Helmet } from 'react-helmet';
 import ComponentMap from '@irvingjs/styled-components';
-import * as materialComponents from '@material-ui/core';
 import AdminBar from '@irvingjs/wp-admin-bar';
 
 // Icons.
 import { Search as SearchIcon } from '@material-ui/icons';
-
-const transformName = (original) => original
-  .replace(/(^[A-Z])/, ([first]) => first.toLowerCase())
-  .replace(/([A-Z])/g, ([letter]) => `-${letter.toLowerCase()}`);
 
 export default {
   ...ComponentMap,
   'irving/admin-bar': AdminBar,
   'irving/helmet': Helmet,
   'material-icon/search': SearchIcon,
-  ...Object.keys(materialComponents)
-    .reduce((acc, key) => {
-      acc[`material/${transformName(key)}`] = materialComponents[key];
-      return { ...acc };
-    },
-    {}),
 };
