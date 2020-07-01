b2df764ba9d908c4eb845d8d8b3b2dfc39757a59
James Burke <james.walter.burke@gmail.com>
2020-06-30 23:26:41 -0400

    Implement theme injection and site theme examples

--- private/irving/componentMap.js
+++ private/irving/componentMap.js
@@ -1,12 +1,13 @@
 import { Helmet } from 'react-helmet';
 import ComponentMap from '@irvingjs/styled-components';
 import AdminBar from '@irvingjs/wp-admin-bar';
+import userThemes from './themes';
 
 // Icons.
 import { FiSearch } from 'react-icons/fi';
 
 export default {
-  ...ComponentMap,
+  ...ComponentMap(userThemes),
   'irving/admin-bar': AdminBar,
   'irving/helmet': Helmet,
   'irving-icon/search': FiSearch,
