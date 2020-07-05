f168bcc2482c146d66c71ffb4714727955120cb0
James Burke <james.walter.burke@gmail.com>
2020-07-05 16:12:40 -0400

    Stuff

--- private/irving/themes/text/heading.js
+++ private/irving/themes/text/heading.js
@@ -4,8 +4,8 @@ import get from 'lodash/get';
 
 // eslint-disable import/prefer-default-export.
 export const TextWrapper = styled.div`
-  border-bottom: 3px solid ${(props) => get(props, 'theme.colors.text.primary')};
-  color: ${(props) => get(props, 'theme.colors.primary.main')};
+  border-bottom: 3px solid ${(props) => get(props, 'theme.colors.primary.main')};
+  color: ${(props) => get(props, 'theme.colors.text.primary')};
   font-size: 2.25rem;
   margin-bottom: 1rem;
   padding-bottom: 1rem;
