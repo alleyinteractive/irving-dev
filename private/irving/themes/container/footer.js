8e7e85fe0048832c785408fb99e623abc46b0f5c
James Burke <james.walter.burke@gmail.com>
2020-07-08 22:48:29 -0400

    Begin refactor

---
+++ private/irving/themes/container/footer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+/* eslint-disable import/prefer-default-export */
+export const ContainerWrapper = styled.div`
+  background-color: #d4d4db;
+  margin: auto;
+  padding: 2rem 0;
+  width: 100%;
+`;
+/* eslint-enable */
--- private/irving/themes/index.js
+++ private/irving/themes/index.js
@@ -1,9 +1,19 @@
 import * as heading from './text/heading';
 import * as subheading from './text/subheading';
+import * as footer from './container/footer';
+// import * as header from './logo/header';
+// import * as footer from './logo/footer';
 
 export default {
+  'irving/container': {
+    footer,
+  },
   'irving/text': {
     heading,
     subheading,
   },
+  // 'irving/logo': {
+  //   header,
+  //   footer,
+  // }
 };
