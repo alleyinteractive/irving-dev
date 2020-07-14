2bbe33d3e63e497fac68285c34ae15148af554a9
Owen Stowe <ostowe@gmail.com>
2020-07-14 10:01:31 -0400

    More testing

--- private/irving/componentMap.js
+++ private/irving/componentMap.js
@@ -1,9 +1,10 @@
-import React from 'react';
+// import React from 'react';
 import { Helmet } from 'react-helmet';
 import ComponentMap from '@irvingjs/styled-components';
 import AdminBar from '@irvingjs/wp-admin-bar';
 import { FiSearch } from 'react-icons/fi';
-import Loadable from 'react-loadable';
+// import Loadable from 'react-loadable';
+import Test from 'components/test';
 import userThemes from './themes';
 
 export default {
@@ -11,8 +12,9 @@ export default {
   'irving/admin-bar': AdminBar,
   'irving/helmet': Helmet,
   'irving-icon/search': FiSearch,
-  'irving/test': Loadable({
-    loader: () => import('./components/test'),
-    loading: () => (<div>...loading</div>),
-  }),
+  'irving/test': Test,
+  // 'irving/test': Loadable({
+  //   loader: () => import('./components/test'),
+  //   loading: () => (<div>...loading</div>),
+  // }),
 };
--- private/irving/irving.config.js
+++ private/irving/irving.config.js
@@ -1,11 +1,9 @@
 import styledComponentsConfig from '@irvingjs/styled';
-import loadableConfig from '@irvingjs/loadable';
 
 const config = {
   name: 'irving-dev-app',
   packages: [
     styledComponentsConfig,
-    loadableConfig,
   ],
 };
 
